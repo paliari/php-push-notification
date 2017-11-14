09fab323f8f0bb32174a205a158f523461cd6e3b
Marcos Paliari <marcos@paliari.com.br>
2017-11-14 19:17:46 -0200

    add src/BasePushNotification.php

---
+++ src/BasePushNotification.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Paliari;
+
+use Paliari\Utils\A;
+
+abstract class BasePushNotification
+{
+
+    protected $base_url;
+
+    /**
+     * @var Api
+     */
+    protected $api;
+
+    protected static $_instances = [];
+
+    /**
+     * @param string $app_name
+     *
+     * @return static
+     */
+    public static function instance($app_name)
+    {
+        $key = get_called_class() . $app_name;
+        if (!isset(static::$_instances[$key])) {
+            static::$_instances[$key] = new static();
+        }
+
+        return static::$_instances[$key];
+    }
+
+    public function init($config)
+    {
+        $this->base_url = A::get($config, 'base_url', $this->base_url);
+        $this->api      = $this->newApi($this->base_url);
+
+        return $this;
+    }
+
+    protected function newApi($base_url)
+    {
+        return new Api($base_url);
+    }
+
+}
